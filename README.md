5a61bed58323364ac3d1c5e5370ab08152913a1f
Andrzej Martyna <andrzej.martyna@gmail.com>
2025-07-25 16:10:32 +0200

    fix: handle keeping file permissions while modifying the contents

--- README.md
+++ README.md
@@ -24,3 +24,11 @@ As big and right it might be - let's start with a few commands and experiment wi
 1. Maybe Makefile could be better then scripts? It would be nice to type "make help" and see what is there inside.
 2. How to integrate scripts with the shell even better (not by PATH but by having alias or something)?
 3. How to provide auto completion to commands and arguments? (note that even without completion you may achieve instant reuse by Ctrl-R and its incremental search)
+
+# Maintenance
+
+In case you need to modify anything within the common part of scripts (all from the begining up to the line containing "COMMON PART ENDS HERE").
+1. Modify `tools/common.txt` file instead of any of other scripts
+1. `cd tools`
+1. `./replace_all.sh`
+1. Review changes `git diff` - should be applied to all scripts
--- tools/replace_common.sh
+++ tools/replace_common.sh
@@ -19,6 +19,14 @@ fi
 # Find the line number of the phrase
 LINE_NUM=$(grep -n "$PHRASE" "$TARGET_FILE" | head -n1 | cut -d: -f1)
 
+# Save original permissions
+if stat -f %A "$TARGET_FILE" >/dev/null 2>&1; then
+    orig_perms=$(stat -f %A "$TARGET_FILE")
+else
+    orig_perms=$(stat -c %a "$TARGET_FILE")
+fi
+echo $orig_perms
+
 # Output common part
 cat "$COMMON_FILE" > temp_replace_common.tmp
 # Output the rest of the file after the phrase
@@ -26,5 +34,10 @@ awk "NR > $LINE_NUM" "$TARGET_FILE" >> temp_replace_common.tmp
 
 # Replace the original file
 mv temp_replace_common.tmp "$TARGET_FILE"
+
+# Restore original permissions
+if command -v chmod >/dev/null; then
+    chmod "$orig_perms" "$TARGET_FILE" 2>/dev/null || true
+fi
 
 echo "Replaced common part in $TARGET_FILE with $COMMON_FILE up to and including line $LINE_NUM." 
